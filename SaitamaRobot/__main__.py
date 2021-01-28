45620f86eec9791cc7d9d65bc26c685c299bea67
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-01-28 05:06:47 -0800

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -100,7 +100,7 @@ And the following:
     "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n",
 )
 
-SAITAMA_IMG = "https://telegra.ph/file/46e6d9dfcb3eb9eae95d9.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/8270aeca345cdefbffaa5.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 Saitama is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
@@ -222,28 +222,21 @@ def start(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                text="☑️ Add Saitama to your group",
-                                url="t.me/{}?startgroup=true".format(
-                                    context.bot.username
-                                ),
+                                text="Muhamad Risman Aziz",
+                                url="https://t.me/mrismanaziz_",
                             )
                         ],
                         [
                             InlineKeyboardButton(
-                                text="🚑 Support Group",
+                                text="🚑 Join Group",
                                 url=f"https://t.me/{SUPPORT_CHAT}",
                             ),
                             InlineKeyboardButton(
-                                text="🔔 Updates Channel",
+                                text="🔔 Subscribe Channel",
                                 url="https://t.me/OnePunchUpdates",
                             ),
                         ],
-                        [
-                            InlineKeyboardButton(
-                                text="🧾 Getting started guide",
-                                url="https://t.me/OnePunchUpdates/29",
-                            )
-                        ],
+                     
                         [
                             InlineKeyboardButton(
                                 text="🗄 Source code",
